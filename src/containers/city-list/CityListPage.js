1af3e965b52dae2df3fd2ee721990b8c908a02ea
Milos Doderovic <mdoderovic@novomaticls.com>
2018-10-31 08:39:19 +0100

    Formatting

--- src/containers/city-list/CityListPage.js
+++ src/containers/city-list/CityListPage.js
@@ -37,74 +37,74 @@ class CityListPage extends Component {
         }
     }
 
-  render()
-  {
-      const {state} = this.props;
+    render()
+    {
+        const {state} = this.props;
 
-      const events = state.events.events ?
-          state.events.events.map( event =>
-              (<div key={event.id}>
-                  <div className="link-container">
-                      <span className="date-time">
-                          {event.local_date} {event.local_time}
-                      </span>
-                      <a href={event.link}>
-                          {event.name}
-                      </a>
-                      <span className="details" onClick={this.handleEventDetailsClick.bind(this, event.id)}>
-                          {
-                              !this.props.state.visibleDetails.includes(event.id) &&
-                              <span>show details</span>
-                          }
-                          {
-                              this.props.state.visibleDetails.includes(event.id) &&
-                              <span>hide details</span>
-                          }
-                      </span>
-                  </div>
-                      {
-                          this.props.state.visibleDetails.includes(event.id) &&
-                          <div className="details-container" dangerouslySetInnerHTML={{__html: event.description}}/>
-                      }
-              </div>)
-          )
-          :
-          (<div/>);
+        const events = state.events.events ?
+            state.events.events.map( event =>
+                (<div key={event.id}>
+                    <div className="link-container">
+                        <span className="date-time">
+                            {event.local_date} {event.local_time}
+                        </span>
+                        <a href={event.link}>
+                            {event.name}
+                        </a>
+                        <span className="details" onClick={this.handleEventDetailsClick.bind(this, event.id)}>
+                            {
+                                !this.props.state.visibleDetails.includes(event.id) &&
+                                <span>show details</span>
+                            }
+                            {
+                                this.props.state.visibleDetails.includes(event.id) &&
+                                <span>hide details</span>
+                            }
+                        </span>
+                    </div>
+                    {
+                        this.props.state.visibleDetails.includes(event.id) &&
+                        <div className="details-container" dangerouslySetInnerHTML={{__html: event.description}}/>
+                    }
+                </div>)
+            )
+            :
+            (<div/>);
 
 
-      const cities = state.cities.map( city =>
-          (<div className="city-container" key={city.id}>
-              <div onClick={this.handleCityClick.bind(this, city)} className="city-name">
-                {city.city}
-              </div>
-              {
-                  state.events.city && state.events.city.id === city.id &&
-                  <div className="events-container">{events}</div>
-              }
+        const cities = state.cities.map( city =>
+            (<div className="city-container" key={city.id}>
+                <div onClick={this.handleCityClick.bind(this, city)} className="city-name">
+                    {city.city}
+                </div>
+                {
+                    state.events.city && state.events.city.id === city.id &&
+                    <div className="events-container">{events}</div>
+                }
           </div>)
-      );
+        );
 
-      return (
-          <div className="list-page-container">
-              <h3>City List</h3>
-              <div>
-                  {cities}
-              </div>
-          </div>
-      )
-  }
+        return (
+            <div className="list-page-container">
+                <h3>City List</h3>
+                <div>
+                    {cities}
+                </div>
+            </div>
+        )
+    }
+
 }
 
-const mapStateToProps = ({list}) => ({
+const mapStateToProps = ({list}) =>
+({
     state: list
 });
 
-function mapDispatchToProps(dispatch)
-{
-    return {
-        actions: bindActionCreators(actions, dispatch)
-    };
-}
+const mapDispatchToProps = (dispatch) =>
+({
+    actions: bindActionCreators(actions, dispatch)
+});
 
 export default connect(
     mapStateToProps,
